04a1769c846117b569591304c1c6235197c0b19a
Noman5713 <noman15-5713@diu.edu.bd>
2025-06-27 16:07:24 +0600

    homepage html

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DIU BeingScholar</title>
+
+    <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
+
+
+</head>
+
+<body>
+    
+    <!--navigation bar-->
+    <nav>
+        <div class="navigation">
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>Courses</a></li>
+                <li><a><b>Login</b></a></li>
+            </ul>
+            
+         </div>
+    </nav>
+
+    <!--Homepage-->
+    <section class="intro">
+        <h1><b>DIU BeingScholar</b></h1>
+        <p><b>Gateway to Education and Opportunities</b></p>
+    </section>
+</body>
+</html>
+
+   

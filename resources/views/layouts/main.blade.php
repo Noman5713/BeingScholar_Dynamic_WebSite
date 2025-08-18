722ecf1f29d05af7f8d21fee4bc9664cd7dd44ab
Noman5713 <noman15-5713@diu.edu.bd>
2025-08-19 00:58:37 +0600

    fix about page and faq page issue

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,9 +19,8 @@
                 <li><a href="/about">About</a></li>
                 <li><a href="/courses">Courses</a></li>
                 <li><a href="/#teachers">Teachers</a></li>
-                <li><a href="/#services">Services</a></li>
+                <li><a href="/faq">FAQ</a></li>
                 <li><a href="/#success">Success & Reviews</a></li>
-                <li><a href="/#join">Join Us</a></li>
                 <li><a href="/contact">Contact</a></li>
             </ul>
             <div class="login-btn"><a href="/login">Login/Register</a></div>

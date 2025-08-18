db0e57a1f0b2cb05a1f69fd44de2f1ccbeb587cf
Noman5713 <noman15-5713@diu.edu.bd>
2025-08-19 01:50:08 +0600

    add teachers and review and success pages

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,9 +18,9 @@
                 <li><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/courses">Courses</a></li>
-                <li><a href="/#teachers">Teachers</a></li>
+                <li><a href="/teachers">Teachers</a></li>
                 <li><a href="/faq">FAQ</a></li>
-                <li><a href="/#success">Success & Reviews</a></li>
+                <li><a href="/success">Success & Reviews</a></li>
                 <li><a href="/contact">Contact</a></li>
             </ul>
             <div class="login-btn"><a href="/login">Login/Register</a></div>
